a02e09c3a94d139bc2a2b7636eed20812b71b0c2
SyedAliZafar <syedalizafar0@gmail.com>
2023-08-02 10:20:58 +0200

    Initial commit

--- README.md
+++ README.md
@@ -91,8 +91,7 @@ open up you local host and port
 
 	
 ## 3. Create ECR repo to store/save docker image
-    - Save the URI: 566373416292.dkr.ecr.us-east-1.amazonaws.com/chicken
-
+		- Save the URI: 634670391015.dkr.ecr.eu-north-1.amazonaws.com/chicken
 	
 ## 4. Create EC2 machine (Ubuntu) 
 
--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -22,13 +22,24 @@ class PrepareCallback:
         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
     
 
+
+    @property
+    def _create_ckpt_callbacks(self):
+        filepath_str = str(self.config.checkpoint_model_filepath)  # Convert to string
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath=filepath_str,
+            save_best_only=True
+        )
+    
+
+    '''
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
             filepath=self.config.checkpoint_model_filepath,
             save_best_only=True
         )
-
+    '''
 
     def get_tb_ckpt_callbacks(self):
         return [
